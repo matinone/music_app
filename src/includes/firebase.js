9d47086f608a35705be0877ba752e452c1b41c80
mbrignone <mnbrignone@gmail.com>
2023-01-17 11:40:41 -0300

    Add initial Firebase setup

---
+++ src/includes/firebase.js
@@ -0,0 +1,13 @@
+import firebase from "firebase/app";
+import "firebase/auth";
+
+const firebaseConfig = {
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: "music-app-7e67f.firebaseapp.com",
+  projectId: "music-app-7e67f",
+  storageBucket: "music-app-7e67f.appspot.com",
+  appId: "1:134144614256:web:151019518e8cd42bfb79b5",
+  measurementId: "G-DT4K79P6CF",
+};
+
+export default firebase.initializeApp(firebaseConfig);
