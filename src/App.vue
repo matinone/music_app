e55edb32cf3e6eddbcf49b41b818914f98c6cf29
mbrignone <mnbrignone@gmail.com>
2023-01-18 18:26:00 -0300

    Persist user authentication if the page is refreshed

--- src/App.vue
+++ src/App.vue
@@ -278,11 +278,26 @@
 import AppHeader from "@/components/AppHeader.vue";
 import AppAuth from "@/components/AppAuth.vue";
 
+import { mapWritableState } from "pinia";
+import useUserStore from "@/stores/user";
+import { auth } from "@/includes/firebase";
+
 export default {
   name: "App",
   components: {
     AppHeader,
     AppAuth,
+  },
+
+  computed: {
+    ...mapWritableState(useUserStore, ["userLoggedIn"]),
+  },
+
+  // lifecycle hook
+  created() {
+    if (auth.currentUser) {
+      this.userLoggedIn = true;
+    }
   },
 };
 </script>
