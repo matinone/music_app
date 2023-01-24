2221011380f74ac9febf95600aea6df0cbcab708
mbrignone <mnbrignone@gmail.com>
2023-01-24 15:03:46 -0300

    Prevent unauthenticated users from accessing manage page
    - Redirect to home page when manually entering the URL.
    - Redirect to home page when logging out while being in the
    manage page.

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -26,10 +26,7 @@
               </router-link>
             </li>
             <li>
-              <a
-                class="px-2 text-white"
-                href="#"
-                @click.prevent="userStore.signOut"
+              <a class="px-2 text-white" href="#" @click.prevent="signOut"
                 >Logout</a
               >
             </li>
@@ -59,6 +56,13 @@ export default {
   methods: {
     toggleAuthModal() {
       this.modalStore.isOpen = !this.modalStore.isOpen;
+    },
+    signOut() {
+      this.userStore.signOut();
+
+      if (this.$route.name === "manage") {
+        this.$router.push({ name: "home" });
+      }
     },
   },
 };
