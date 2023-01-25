e7b8af005f993b9646aea5dd4593d1ec5ae9d520
mbrignone <mnbrignone@gmail.com>
2023-01-25 17:49:33 -0300

    Add initial handling of files drag and drop events

--- src/components/UploadFiles.vue
+++ src/components/UploadFiles.vue
@@ -8,6 +8,16 @@
       <!-- Upload Dropbox -->
       <div
         class="w-full px-10 py-20 rounded text-center cursor-pointer border border-dashed border-gray-400 text-gray-400 transition duration-500 hover:text-white hover:bg-green-400 hover:border-green-400 hover:border-solid"
+        :class="{
+          'bg-green-400 hover:border-green-400 hover:border-solid': is_dragover,
+        }"
+        @drag.prevent.stop=""
+        @dragstart.prevent.stop=""
+        @dragend.prevent.stop="is_dragover = false"
+        @dragover.prevent.stop="is_dragover = true"
+        @dragenter.prevent.stop="is_dragover = true"
+        @dragleave.prevent.stop="is_dragover = false"
+        @drop.prevent.stop="uploadFile($event)"
       >
         <h5>Drop your files here</h5>
       </div>
@@ -49,5 +59,25 @@
 <script>
 export default {
   name: "UploadFiles",
+  data() {
+    return {
+      is_dragover: false,
+    };
+  },
+  methods: {
+    uploadFile($event) {
+      this.is_dragover = false;
+
+      // convert object to array
+      const files = [...$event.dataTranfer.files];
+      files.forEach((file) => {
+        if (file.type !== "audio/mpeg") {
+          return; // end current iteration
+        }
+
+        // upload file to Firebase
+      });
+    },
+  },
 };
 </script>
